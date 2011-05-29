5e7f17b39e0a2d1261c37ad552ec2c51a18a0e65
Matt Boulanger <celeodor@gmail.com>
2011-05-28 21:42:41 -0700

    Fixing typos

--- src/net/thedarktide/celeo/friendlist/LoginListener.java
+++ src/net/thedarktide/celeo/friendlist/LoginListener.java
@@ -1,6 +1,6 @@
 /*
  * FriendList
- * Copyright (C) 2010 Celeo <celeodor at gmail dot com>
+ * Copyright (C) 2011 Celeo <celeodor@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
