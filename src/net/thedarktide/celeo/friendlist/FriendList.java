7aa4c4a0663395161be6fea3b6e59617be8f7bc1
Matt Boulanger <celeodor@gmail.com>
2011-05-31 21:37:42 -0700

    Changes for May 31

--- src/net/thedarktide/celeo/friendlist/FriendList.java
+++ src/net/thedarktide/celeo/friendlist/FriendList.java
@@ -81,12 +81,11 @@ public class FriendList extends JavaPlugin {
 			Player player = (Player)sender;
 			
 			// === Friends ===
-			if((commandLabel.equalsIgnoreCase("friend") || commandLabel.equalsIgnoreCase("friendlist")
-					|| commandLabel.equalsIgnoreCase("friends")) && args.length >= 0)
+			if(commandLabel.equalsIgnoreCase("friend") || commandLabel.equalsIgnoreCase("friendlist")
+					|| commandLabel.equalsIgnoreCase("friends"))
 			{
 				//blank / -h / -help
-				
-				if(args[0].equalsIgnoreCase("") || args[0].equalsIgnoreCase("-h") || args[0].equalsIgnoreCase("-help"))
+				if(args.length == 0)
 				{
 					player.sendMessage(ChatColor.GOLD + "Friend/Enemy List Commands:");
 					player.sendMessage(ChatColor.BLACK + "==================================================");
@@ -99,201 +98,265 @@ public class FriendList extends JavaPlugin {
 					player.sendMessage(cgray + "-clear" + cwhite + " : Clears your list");
 					player.sendMessage(ChatColor.BLACK + "==================================================");
 				}
-				
-				//-see / -view / -show
-				if(args[0].equalsIgnoreCase("-see") || args[0].equalsIgnoreCase("-view") || args[0].equalsIgnoreCase("-show") || args[0].equalsIgnoreCase("-list"))
+				else if(args.length >= 1)
 				{
-					if(Util.friendList.get(player.getDisplayName()) != null)
+					if(args[0].equalsIgnoreCase("-h") || args[0].equalsIgnoreCase("-help"))
 					{
-						String list = cgreen + "Friend list: " + cwhite;
-						for(String str : Util.friendList.get(player.getDisplayName()))
+						player.sendMessage(ChatColor.GOLD + "Friend/Enemy List Commands:");
+						player.sendMessage(ChatColor.BLACK + "==================================================");
+						player.sendMessage(cgray + "/friend|s|list or /enemy|s|list");
+						player.sendMessage(cgray + "-see / -show / -view" + cwhite + " : Shows the list");
+						player.sendMessage(cwhite + "Offline players are in white, while online players will be in");
+						player.sendMessage(cgreen + "green " + cwhite + "or " + cred + "red" + cwhite + ", depending on which list they belong to.");
+						player.sendMessage(cgray + "-a|dd [name(s)]" + cwhite + " : Adds the people to your list");
+						player.sendMessage(cgray + "-rem|ove [name(s)]" + cwhite + " : Removes the people from your list");
+						player.sendMessage(cgray + "-clear" + cwhite + " : Clears your list");
+						player.sendMessage(ChatColor.BLACK + "==================================================");
+					}
+					//-see / -view / -show
+					if(args[0].equalsIgnoreCase("-see") || args[0].equalsIgnoreCase("-view") || args[0].equalsIgnoreCase("-show") || args[0].equalsIgnoreCase("-list"))
+					{
+						if(Util.friendList.get(player.getDisplayName()) != null)
 						{
-							if(isPlayerOnline(server.getPlayer(str), server))
-								list += cgreen + str + " ";
+							String list = cgreen + "Friend list: " + cwhite;
+							for(String str : Util.friendList.get(player.getDisplayName()))
+							{
+								if(isPlayerOnline(server.getPlayer(str), server))
+									list += cgreen + str + " ";
+								else
+									list += cwhite + str + " ";
+							}
+							if(list != "" || list != " ")
+								player.sendMessage(list);
 							else
-								list += cwhite + str + " ";
+								player.sendMessage(cred + "You don't have any people in your friend list.");
 						}
-						if(list != "" || list != " ")
-							player.sendMessage(list);
 						else
-							player.sendMessage(cred + "You don't have any people in your friend list.");
-					}
-					else
-					{
-						player.sendMessage(cred + "You don't have any people in your friend list.");
-					}
-				}
-				//-a|dd
-				if(args[0].equalsIgnoreCase("-a") || args[0].equalsIgnoreCase("-add"))
-				{
-					if(args.length >= 1)
-					{
-						if(Util.enemyList.get(player.getDisplayName()).contains(args[1]))
 						{
-							player.sendMessage(cred + "You cannot add an enemy to your friend list.");
+							player.sendMessage(cred + "You don't have any people in your friend list.");
 						}
-						else
+					}
+					//-a|dd
+					if(args[0].equalsIgnoreCase("-a") || args[0].equalsIgnoreCase("-add"))
+					{
+						if(args.length >= 1)
+						{
+							if(Util.enemyList.get(player.getDisplayName()).contains(args[1]))
+							{
+								player.sendMessage(cred + "You cannot add an enemy to your friend list.");
+							}
+							else
+							{
+								if(!Util.friendList.containsKey(player.getDisplayName()))
+								{
+									ArrayList<String> temp = new ArrayList<String>();
+									Util.friendList.put(player.getDisplayName(), temp);
+								}
+								ArrayList<String> temp = Util.friendList.get(player.getDisplayName());
+								ArrayList<String> playersAdded = new ArrayList<String>();
+								if(temp != null)
+								{
+									for(int i = 1; i < args.length; i++)
+								{
+									temp.add(args[i]);
+									playersAdded.add(args[i]);
+								}
+								Util.friendList.put(player.getDisplayName(), temp);
+								player.sendMessage(ChatColor.GRAY + (playersAdded + " added to your friends list."));
+								Util.config.setProperty("friend." + player.getDisplayName(), Util.friendList.get(player.getDisplayName()));
+								Util.config.save();
+								}
+							}
+						}
+					}
+					//-r|em|ove
+					if(args[0].equalsIgnoreCase("-r") || args[0].equalsIgnoreCase("-rem") || args[0].equalsIgnoreCase("-remove"))
+					{
+						if(args.length >= 1)
 						{
 							if(!Util.friendList.containsKey(player.getDisplayName()))
 							{
 								ArrayList<String> temp = new ArrayList<String>();
 								Util.friendList.put(player.getDisplayName(), temp);
+								player.sendMessage(cred + "You do not have anyone in your friend list.");
 							}
-							ArrayList<String> temp = Util.friendList.get(player.getDisplayName());
-							ArrayList<String> playersAdded = new ArrayList<String>();
-							if(temp != null)
+							else
 							{
-								for(int i = 1; i < args.length; i++)
-							{
-								temp.add(args[i]);
-								playersAdded.add(args[i]);
-							}
-							Util.friendList.put(player.getDisplayName(), temp);
-							player.sendMessage(ChatColor.GRAY + (playersAdded + " added to your friends list."));
+								ArrayList<String> temp = Util.friendList.get(player.getDisplayName());
+								ArrayList<String> playersRemoved = new ArrayList<String>();
+								if(temp != null)
+								{
+									for(int i = 1; i < args.length; i++)
+									{
+										if(Util.friendList.get(player.getDisplayName()).contains(args[i]))
+										{
+											temp.remove(args[i]);
+											playersRemoved.add(args[i]);
+										}
+									}
+									Util.friendList.put(player.getDisplayName(), temp);
+									if(playersRemoved.size() >= 0)
+									{
+										player.sendMessage(cgray + (playersRemoved.toString() + " removed from your friend list."));
+									}
+									else
+									{
+										player.sendMessage(cgray + "None of the players you specified was on your friend list.");
+									}
+									Util.config.setProperty("friend." + player.getDisplayName(), Util.friendList.get(player.getDisplayName()));
+									Util.config.save();
+								}
 							}
 						}
 					}
-				}
-				//-rem|ove
-				if(args[0].equalsIgnoreCase("-rem") || args[0].equalsIgnoreCase("-remove"))
-				{
-					if(args.length >= 1)
+					//-clear
+					if(args[0].equalsIgnoreCase("-clear"))
 					{
-						if(!Util.friendList.containsKey(player.getDisplayName()))
+						try
 						{
 							ArrayList<String> temp = new ArrayList<String>();
+							temp.add("");
 							Util.friendList.put(player.getDisplayName(), temp);
+							player.sendMessage(cred + "Your friend list has been cleared.");
+						}
+						catch (Exception ex)
+						{
 							player.sendMessage(cred + "You do not have anyone in your friend list.");
 						}
-						else
-						{
-							ArrayList<String> temp = Util.friendList.get(player.getDisplayName());
-							ArrayList<String> playersRemoved = new ArrayList<String>();
-							if(temp != null)
-							{
-								for(int i = 1; i < args.length; i++)
-							{
-								temp.remove(args[i]);
-								playersRemoved.add(args[i]);
-							}
-							Util.friendList.put(player.getDisplayName(), temp);
-							player.sendMessage(ChatColor.GRAY + (playersRemoved + " removed to your friends list."));
-							}
-						}
-					}
-				}
-				//-clear
-				if(args[0].equalsIgnoreCase("-clear"))
-				{
-					try
-					{
-						Util.friendList.put(player.getDisplayName(), null);
-						player.sendMessage(cred + "Your friend list has been cleared.");
-					}
-					catch (Exception ex)
-					{
-						player.sendMessage(cred + "You do not have anyone in your friend list.");
 					}
 				}
 			}
 			
 			//=== Enemies ===
-			if((commandLabel.equalsIgnoreCase("enemy") || commandLabel.equalsIgnoreCase("enemylist")
-					|| commandLabel.equalsIgnoreCase("enemies")) && args.length >= 0)
+			if(commandLabel.equalsIgnoreCase("enemy") || commandLabel.equalsIgnoreCase("enemylist")
+					|| commandLabel.equalsIgnoreCase("enemies"))
 			{
-				//-see / -view / -show
-				if(args[0].equalsIgnoreCase("-see") || args[0].equalsIgnoreCase("-view") || args[0].equalsIgnoreCase("-show") || args[0].equalsIgnoreCase("-list"))
+				//blank / -h / -help
+				if(args.length == 0)
 				{
-					if(Util.enemyList.get(player.getDisplayName()) != null)
-					{
-						String list = cred + "Enemy list: " + cwhite;
-						for(String str : Util.enemyList.get(player.getDisplayName()))
-						{
-							if(isPlayerOnline(server.getPlayer(str), server))
-								list += cred + str + " ";
-							else
-								list += cwhite + str + " ";
-						}
-						if(list != "" || list != " ")
-							player.sendMessage(list);
-						else
-							player.sendMessage(cred + "You don't have any people in your enemy list.");
-					}
-					else
-					{
-						player.sendMessage(cred + "You don't have any people in your enemy list.");
-					}
+					player.sendMessage(ChatColor.GOLD + "Friend/Enemy List Commands:");
+					player.sendMessage(ChatColor.BLACK + "==================================================");
+					player.sendMessage(cgray + "/friend|s|list or /enemy|s|list");
+					player.sendMessage(cgray + "-see / -show / -view" + cwhite + " : Shows the list");
+					player.sendMessage(cwhite + "Offline players are in white, while online players will be in");
+					player.sendMessage(cgreen + "green " + cwhite + "or " + cred + "red" + cwhite + ", depending on which list they belong to.");
+					player.sendMessage(cgray + "-a|dd [name(s)]" + cwhite + " : Adds the people to your list");
+					player.sendMessage(cgray + "-rem|ove [name(s)]" + cwhite + " : Removes the people from your list");
+					player.sendMessage(cgray + "-clear" + cwhite + " : Clears your list");
+					player.sendMessage(ChatColor.BLACK + "==================================================");
 				}
-				//-a|dd
-				if(args[0].equalsIgnoreCase("-a") || args[0].equalsIgnoreCase("-add"))
+				else if(args.length >= 1)
 				{
-					if(args.length >= 1)
+					//-see / -view / -show
+					if(args[0].equalsIgnoreCase("-see") || args[0].equalsIgnoreCase("-view") || args[0].equalsIgnoreCase("-show") || args[0].equalsIgnoreCase("-list"))
 					{
-						if(Util.friendList.get(player.getDisplayName()).contains(args[1]))
+						if(Util.enemyList.get(player.getDisplayName()) != null)
 						{
-							player.sendMessage(cred + "You cannot add an friend to your enemy list.");
+							String list = cred + "Enemy list: " + cwhite;
+							for(String str : Util.enemyList.get(player.getDisplayName()))
+							{
+								if(isPlayerOnline(server.getPlayer(str), server))
+									list += cred + str + " ";
+								else
+									list += cwhite + str + " ";
+							}
+							if(list != "" || list != " ")
+								player.sendMessage(list);
+							else
+								player.sendMessage(cred + "You don't have any people in your enemy list.");
 						}
 						else
+						{
+							player.sendMessage(cred + "You don't have any people in your enemy list.");
+						}
+					}
+					//-a|dd
+					if(args[0].equalsIgnoreCase("-a") || args[0].equalsIgnoreCase("-add"))
+					{
+						if(args.length >= 1)
+						{
+							if(Util.friendList.get(player.getDisplayName()).contains(args[1]))
+							{
+								player.sendMessage(cred + "You cannot add a friend to your enemy list.");
+							}
+							else
+							{
+								if(!Util.enemyList.containsKey(player.getDisplayName()))
+								{
+									ArrayList<String> temp = new ArrayList<String>();
+									Util.enemyList.put(player.getDisplayName(), temp);
+								}
+								ArrayList<String> temp = Util.enemyList.get(player.getDisplayName());
+								ArrayList<String> playersAdded = new ArrayList<String>();
+								if(temp != null)
+								{
+									for(int i = 1; i < args.length; i++)
+								{
+									temp.add(args[i]);
+									playersAdded.add(args[i]);
+								}
+								Util.enemyList.put(player.getDisplayName(), temp);
+								player.sendMessage(ChatColor.GRAY + (playersAdded + " added to your enemy list."));
+								Util.config.setProperty("enemy." + player.getDisplayName(), Util.friendList.get(player.getDisplayName()));
+								Util.config.save();
+								}
+							}
+						}
+					}
+					//-r|em|ove
+					if(args[0].equalsIgnoreCase("-r") || args[0].equalsIgnoreCase("-rem") || args[0].equalsIgnoreCase("-remove"))
+					{
+						if(args.length >= 1)
 						{
 							if(!Util.enemyList.containsKey(player.getDisplayName()))
 							{
 								ArrayList<String> temp = new ArrayList<String>();
 								Util.enemyList.put(player.getDisplayName(), temp);
+								player.sendMessage(cred + "You do not have anyone in your enemy list.");
 							}
-							ArrayList<String> temp = Util.enemyList.get(player.getDisplayName());
-							ArrayList<String> playersAdded = new ArrayList<String>();
-							if(temp != null)
+							else
 							{
-								for(int i = 1; i < args.length; i++)
-							{
-								temp.add(args[i]);
-								playersAdded.add(args[i]);
-							}
-							Util.enemyList.put(player.getDisplayName(), temp);
-							player.sendMessage(ChatColor.GRAY + (playersAdded + " added to your enemy list."));
+								ArrayList<String> temp = Util.enemyList.get(player.getDisplayName());
+								ArrayList<String> playersRemoved = new ArrayList<String>();
+								if(temp != null)
+								{
+									for(int i = 1; i < args.length; i++)
+									{
+										if(Util.enemyList.get(player.getDisplayName()).contains(args[i]))
+										{
+											temp.remove(args[i]);
+											playersRemoved.add(args[i]);
+										}
+									}
+									Util.enemyList.put(player.getDisplayName(), temp);
+									if(playersRemoved.size() >= 0)
+									{
+										player.sendMessage(cgray + (playersRemoved.toString() + " removed from your enemy list."));
+									}
+									else
+									{
+										player.sendMessage(cgray + "None of the players you specified was on your enemy list.");
+									}
+									Util.config.setProperty("enemy." + player.getDisplayName(), Util.enemyList.get(player.getDisplayName()));
+									Util.config.save();
+								}
 							}
 						}
 					}
-				}
-				//-rem|ove
-				if(args[0].equalsIgnoreCase("-rem") || args[0].equalsIgnoreCase("-remove"))
-				{
-					if(args.length >= 1)
+					//-clear
+					if(args[0].equalsIgnoreCase("-clear"))
 					{
-						if(!Util.enemyList.containsKey(player.getDisplayName()))
+						try
 						{
 							ArrayList<String> temp = new ArrayList<String>();
+							temp.add("");
 							Util.enemyList.put(player.getDisplayName(), temp);
+							player.sendMessage(cred + "Your enemy list has been cleared.");
+						}
+						catch (Exception ex)
+						{
 							player.sendMessage(cred + "You do not have anyone in your enemy list.");
 						}
-						else
-						{
-							ArrayList<String> temp = Util.enemyList.get(player.getDisplayName());
-							ArrayList<String> playersRemoved = new ArrayList<String>();
-							if(temp != null)
-							{
-								for(int i = 1; i < args.length; i++)
-							{
-								temp.remove(args[i]);
-								playersRemoved.add(args[i]);
-							}
-							Util.enemyList.put(player.getDisplayName(), temp);
-							player.sendMessage(ChatColor.GRAY + (playersRemoved + " removed to your enemy list."));
-							}
-						}
-					}
-				}
-				//-clear
-				if(args[0].equalsIgnoreCase("-clear"))
-				{
-					try
-					{
-						Util.enemyList.put(player.getDisplayName(), null);
-						player.sendMessage(cred + "Your enemy list has been cleared.");
-					}
-					catch (Exception ex)
-					{
-						player.sendMessage(cred + "You do not have anyone in your enemy list.");
 					}
 				}
 			}
